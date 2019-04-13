9ce193e898043d139ba44ab2d82679e5dff68b56
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-04-13 08:21:23 -0500

    added some hardcode

--- javascripts/components/store.js
+++ javascripts/components/store.js
@@ -5,7 +5,7 @@ import condiments from '../helpers/condiments.js';
 import sandwich from './sandwich.js';
 import util from '../helpers/util.js';
 
-
+//do they not work if put out here? test
 const meatsInfo = meats.getMeats();
 const cheesesInfo = cheeses.getCheeses();
 const breadsInfo = breads.getBreads();
@@ -24,9 +24,17 @@ const makeStore = () => {
     domString += `<h3>${bookInfo.price}</h3>`;
     domString += `<img src=${bookInfo.image} alt="book cover">`;
     domString += `<button id="cart-button" class="btn btn-danger">Add to Cart</button>`;
+
+
     util.printToDom("store-container", domString);
     document.getElementById('cart-button').addEventListener('click', addToCartEvent);
 };
 
+// const addClickyBtns = () => {$('input[type="checkbox"]').on('change', function() {
+//     $('input[name="' + this.name + '"]').not(this).prop('checked', false);
+// });
+// };
 
 export default { makeStore };
+
+// , addClickyBtns
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,8 @@
-// import store from './components/store.js'
+import store from './components/store.js'
 
 const init = () => {
-    console.log("this thing on?");
-    // store.makeStore();
+    store.makeStore();
+    // store.addClickyBtns();
 };
 
 init();
